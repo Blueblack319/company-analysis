76e8f29727a31110d3c84f54d609ff4edeef36c3
crazybirdz <didwogns789@yonsei.ac.kr>
2021-03-21 09:12:59 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# TODO: Input shareholder equity, ROE, required ROE
+# TODO: make function called get_corp_val
+# TODO: make function called get_proper_stock
+# TODO: Print corp_val and proper_stock
+
+# TODO: Scraping kisrating.com -> Get required ROE
+
